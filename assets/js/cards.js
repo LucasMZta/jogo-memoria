89faf54fdaa6b6fdffa534c8acb83697d23271cc
Lucas Martins <lucas2zonta@hotmail.com>
2023-02-25 15:34:31 -0300

    Atualizando o cron

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -3,12 +3,12 @@ let cards = [
     {id: 2, name: 'Vedita', image: 'assets/images/vedita.png'},
     {id: 3, name: 'Mestre Kame', image: 'assets/images/kame.jpg'},
     {id: 4, name: 'Karin', image: 'assets/images/karin.webp'},
-    {id: 5, name: 'Goku', image: 'assets/images/goku.webp'},
-    {id: 6, name: 'Goku', image: 'assets/images/goku.webp'},
-    {id: 7, name: 'Goku', image: 'assets/images/goku.webp'},
-    {id: 8, name: 'Goku', image: 'assets/images/goku.webp'},
-    {id: 9, name: 'Goku', image: 'assets/images/goku.webp'},
-    {id: 10, name: 'Goku', image: 'assets/images/goku.webp'},
-    {id: 11, name: 'Goku', image: 'assets/images/goku.webp'},
-    {id: 12, name: 'Goku', image: 'assets/images/goku.webp'}
+    {id: 5, name: 'Gohan', image: 'assets/images/gohan.jpg'},
+    {id: 6, name: 'Trunks', image: 'assets/images/trunks.jpg'},
+    {id: 7, name: 'Goten', image: 'assets/images/goten.webp'},
+    {id: 8, name: 'Picolo', image: 'assets/images/picolo.webp'},
+    {id: 9, name: 'Bulma', image: 'assets/images/bulma.jpg'},
+    {id: 10, name: 'Chichi', image: 'assets/images/chichi.jpg'}
+    // {id: 11, name: 'Goku', image: 'assets/images/goku.webp'},
+    // {id: 12, name: 'Goku', image: 'assets/images/goku.webp'}
 ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
 let opt = [];
+let moves = 0;
+let player = '';
+
+document.querySelector('.btn').addEventListener('click', startGame);
+
+function startGame(e) {
+    e.preventDefault();
+    player = document.querySelector('.input').value;
+
+    document.querySelector('.center.start').style.display = 'none';
+    document.querySelector('.center.playing').style.display = 'block';
+
+    generateCards();
+}
 
 function flip(e) {
     let card = e.currentTarget; 
@@ -8,15 +22,19 @@ function flip(e) {
     } else {
         opt.push(card.getAttribute('data-card'));
         card.classList.add('rotate-card');
-
         if(opt[0] == opt[1]) {
-            console.log('Os dois sao iguais');
+            cardsRight(opt[0]);
         } else {
-            // console.log('Os dois sao diferentes');
+            if(opt.length == 2) {
+                setTimeout(cardsWrong, 500);
+            }  
         }
     }
 }
 function generateCards() {
+    document.querySelector('.info-player span').innerHTML = player;
+    startCron();
+
     const duplicate = [...cards, ...cards];
     const suffle = duplicate.sort(() => Math.random() - 0.5);
     suffle.forEach((item)=> {
@@ -32,15 +50,62 @@ function generateCards() {
         let divFaceBack = document.createElement('div');
         divFaceBack.classList.add('face','card-back');
         div.appendChild(divFaceBack);
-
         document.querySelector('.cards').appendChild(div);
         
 
         document.querySelectorAll('.card').forEach(card =>{
             card.addEventListener('click', flip);
         });
-
     });
-
 };
-generateCards();
+function  startCron() {
+    let hour = 0;
+    let minute = 0;
+    let seconds = 0;
+    let time = document.querySelector('.info-timer span');
+
+    setInterval(() => {
+        if(seconds === 59) {
+            minute ++;
+            seconds = 0;
+        } else {
+            seconds ++;
+        }
+        time.innerHTML = formatTime(minute, seconds);
+    }, 1000);
+}
+function formatTime(min, sec) {
+    if(sec < 10) {
+        sec = `0${sec}`;
+    } else {
+        sec = sec;
+    }
+    if(min < 10) {
+        min = `0${min}`
+    } else {
+        min = min;
+    }
+    return `${min}:${sec}`;
+}
+function cardsRight(option) {
+    document.querySelectorAll('.card').forEach(card => {
+        if(card.getAttribute('data-card') == option) {
+            card.classList.add('done');
+            card.querySelector('.card-front').classList.add('disabled');
+            opt = [];
+        };
+    });
+    addMoves();
+};
+function cardsWrong() {
+    document.querySelectorAll('.card.rotate-card:not(.done)').forEach(card =>{
+        card.classList.remove('rotate-card');
+        opt = [];
+    });
+    addMoves();
+};
+function addMoves() {
+    moves ++;
+    console.log(moves);
+    document.querySelector('.info-moves span').innerHTML = moves;
+}
